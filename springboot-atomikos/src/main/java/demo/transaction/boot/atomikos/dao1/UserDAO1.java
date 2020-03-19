497135a3227d4720ec3115ad00d4c2665c2353b2
dean.lee <dean.lee@aliyun.com>
2020-03-19 21:56:03 +0800

    springboot-atomikos

---
+++ springboot-atomikos/src/main/java/demo/transaction/boot/atomikos/dao1/UserDAO1.java
@@ -0,0 +1,10 @@
+package demo.transaction.boot.atomikos.dao1;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+public interface UserDAO1 {
+
+    @Update("update user set name = #{name} where id = #{id}")
+    int updateById(@Param("name") String name, @Param("id") Long id);
+}
---
+++ springboot-atomikos/src/main/java/demo/transaction/boot/atomikos/dao2/UserDAO2.java
@@ -0,0 +1,10 @@
+package demo.transaction.boot.atomikos.dao2;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+public interface UserDAO2 {
+
+    @Update("update user set name = #{name} where id = #{id}")
+    int updateById(@Param("name") String name, @Param("id") Long id);
+}
